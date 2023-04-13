07da098b6c48ee1e9fa6b607db65602983613ae8
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-04-13 20:19:37 +0200

    Add heading of the app with styling

---
+++ task-2/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-title-container">
+    <h1 class="app-title">Toodly</h1>
+    <p class="app-subtitle">Create tasks in seconds.</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.app-title {
+  font-size: 64px;
+  font-weight: 400;
+  text-align: center;
+  padding-top: 20px;
+  background: linear-gradient(90deg, #26e3c2 0%, #45a0f5 104.02%);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.app-subtitle {
+  text-align: center;
+  color: #fcfcfc;
+  font-size: 14px;
+}
+</style>
