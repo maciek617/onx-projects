383fa7a5ddd1075fb6c2b446597ab28b089c5946
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-04-14 08:53:31 +0200

    Add component to show if there is no task available

---
+++ task-2/src/components/NoTaskFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <p v-if="!taskLength" class="no-task-info">No task available.</p>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  taskLength: number;
+}>();
+</script>
+
+<style scoped>
+.no-task-info {
+  color: #fcfcfc;
+  text-align: center;
+}
+</style>
