067122bbe182e24cd5b5fdc02f17d2c8d665e33a
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-04-14 12:56:57 +0200

    render components in main page

--- task-2/src/App.vue
+++ task-2/src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import AddTaskForm from './components/AddTaskForm.vue';
+import AllTasksBoard from './components/AllTasksBoard.vue';
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <AddTaskForm />
+  <div class="container">
+    <Header />
+    <AddTaskForm />
+    <AllTasksBoard />
+  </div>
 </template>
 
 <style scoped></style>
