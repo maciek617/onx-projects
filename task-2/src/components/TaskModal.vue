d702b0c1123ae0faa24639744ba121211df68bc3
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-04-14 09:03:26 +0200

    Split task component on smaller components

---
+++ task-2/src/components/TaskModal.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="modal-wrapper">
+    <p class="modal-task-name">
+      {{ item.name }}
+    </p>
+
+    <button
+      @click="
+        {
+          emit('updateTaskStatusToProgress');
+          emit('closeModal');
+        }
+      "
+      class="modal-button"
+    >
+      Set to in progress
+    </button>
+
+    <div @click="emit('closeModal')" class="modal-button-close">X</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TaskArrayItem from '../interfaces';
+
+defineProps<{
+  item: TaskArrayItem;
+}>();
+
+const emit = defineEmits(['closeModal', 'updateTaskStatusToProgress']);
+</script>
+
+<style scoped>
+.modal-wrapper {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #0f0f0f;
+  color: #fcfcfc;
+  max-width: 600px;
+  width: 100%;
+  padding: 10px 0;
+}
+
+.modal-task-name {
+  text-align: center;
+  padding: 20px 0;
+}
+
+.modal-button {
+  border: 0;
+  outline: 0;
+  border-radius: 4px;
+  background-color: #1e1e1e;
+  padding: 14px 42px;
+  color: #fcfcfc;
+  margin: 0 auto;
+  display: block;
+}
+
+.modal-button:focus {
+  outline: 1px solid #26e3c2;
+}
+
+.modal-button-close {
+  position: absolute;
+  top: 5%;
+  right: 3%;
+}
+</style>
---
+++ task-2/src/components/TaskTools.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="icons-wrapper">
+    <div @click="emit('editTaskStatusToComplete')" class="single-icon-wrapper">
+      <img src="../assets/Vector (8).svg" alt="rubber bin" />
+    </div>
+    <div class="single-icon-wrapper">
+      <img src="../assets/Vector (7).svg" alt="rubber bin" />
+    </div>
+    <div @click="emit('removeCurrentTask')" class="single-icon-wrapper">
+      <img src="../assets/Vector (6).svg" alt="rubber bin" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['editTaskStatusToComplete', 'removeCurrentTask']);
+</script>
+
+<style scoped>
+.icons-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+}
+.single-icon-wrapper {
+  background-color: #1e1e1e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 42px;
+  height: 30px;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.single-icon-wrapper:hover img {
+  transform: translateY(-2px);
+  transition: transform 0.2s ease-in-out;
+}
+</style>
